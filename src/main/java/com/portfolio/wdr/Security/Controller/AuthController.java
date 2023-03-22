899292ae45b4c5d1934a0d403dc4c4453ef9810e
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-03-22 10:27:24 -0300

    en consola del koyeb arroja esto:Connection is not available, request timed out after 31498ms. Se agrega en application.properties spring.datasource.hikari.maxLifeTime : 300000

--- src/main/java/com/portfolio/wdr/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/wdr/Security/Controller/AuthController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@RequestMapping("/auth")
-@CrossOrigin
+@CrossOrigin(origins = "/**")
 // @CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
 
