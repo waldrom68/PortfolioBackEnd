a296b85cab49f2a27d639856b210555c02b2780b
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-03-22 23:35:25 -0300

    LPM

--- src/main/java/com/portfolio/wdr/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/wdr/Security/Controller/AuthController.java
@@ -35,7 +35,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@RequestMapping("/auth")
-@CrossOrigin(origins = { "http://localhost:4200/", "https://portfolio-frontend-wdr.web.app"})
+@CrossOrigin(origins = { "http://localhost:4200/", "https://portfolio-frontend-wdr.web.app/"})
 // @CrossOrigin(origins = "http://localhost:4200/")
 public class AuthController {
 
