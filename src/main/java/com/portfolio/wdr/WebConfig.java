aa7a53a88f2a1b4188cb403937376aeddf0f8f61
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-03-24 19:24:24 -0300

    CORS201

--- src/main/java/com/portfolio/wdr/WebConfig.java
+++ src/main/java/com/portfolio/wdr/WebConfig.java
@@ -19,8 +19,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-        registry.addMapping("https://portfolio-frontend-wdr.web.app")
+//        registry.addMapping("https://portfolio-frontend-wdr.web.app")
+        registry.addMapping("/**")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedOrigins("*")
             .allowedHeaders("*");
