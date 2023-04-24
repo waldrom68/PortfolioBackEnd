ba064e3a79854bf92a99f7b1385536d80cce1188
Walter Romero <95034089+waldrom68@users.noreply.github.com>
2023-04-24 13:32:52 -0300

    CORS reestablecidos, se conserva server.address=0.0.0.0

--- src/main/java/com/portfolio/wdr/WebConfig.java
+++ src/main/java/com/portfolio/wdr/WebConfig.java
@@ -21,7 +21,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//            .allowedOrigins("http://localhost:4200" )
+            .allowedOrigins("http://localhost:4200" )
             .allowedOrigins("https://portfolio-frontend-wdr.web.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
